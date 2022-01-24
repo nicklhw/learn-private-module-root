af44d5e1be012f181a15ce5e4cd29af56a8338aa
nicklhw <nicklhw@gmail.com>
2022-01-24 14:13:32 -0500

    wip

--- main.tf
+++ main.tf
@@ -1,11 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+  cloud {
+    organization = "nw-tfc-learn"
+
+    workspaces {
+      name = "sentinel-td-demo"
+    }
+  }
+}
+
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
+}
+
+provider "random" {}
+
+resource "random_pet" "pet_name" {
+  length = 2
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
-  version = "1.0.0"
+  source = "app.terraform.io/nw-tfc-learn/s3-webapp/aws"
+  name   = "nwong"
+  region = "us-east-1"
+  prefix = "dev"
+  #  prefix  = join("_", [var.prefix, upper(random_pet.pet_name.id)])
+  mal_code = "app5"
+  version  = "1.0.2"
 }
